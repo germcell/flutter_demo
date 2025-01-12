413bf3cf43e4c4e5b1bf7cfe0db2c4c098b3b316
germ <zzengsh@163.com>
2025-01-12 13:48:28 +0800

    feat: 分类页面布局_未封装前

--- lib/pages/tab_page.dart
+++ lib/pages/tab_page.dart
@@ -10,7 +10,7 @@ import 'notify/notify_page.dart';
 /// 底部导航栏，当有导航栏后需要将此页面设置为路由的首页
 /// TODO 封装导航组件，如果有多个页面需要使用底部导航栏，可以复用
 /// TODO 封装思路：通过传入参数来设置底部导航栏的样式和页面
-/// https://www.bilibili.com/video/BV1Qb421Y7SV?spm_id_from=333.788.player.switch&vd_source=576f401fe95063f8ec1d45c1e06864c9&p=17
+/// https://www.bilibili.com/video/BV1Qb421Y7SV?spm_id_from=333.788.player.switch&vd_source=2476f401fe924063f8ec1d424c1e06864c9&p=17
 class TabPage extends StatefulWidget {
   const TabPage({super.key});
 
@@ -46,9 +46,9 @@ class _TabPageState extends State<TabPage> {
             currentIndex: _selectedIndex,
             type: BottomNavigationBarType.fixed,
             // 选中时的字体样式
-            selectedLabelStyle: TextStyle(fontSize: 14, color: Colors.black),
+            selectedLabelStyle: TextStyle(fontSize: 13, color: Colors.black),
             // 未选中时的字体样式
-            unselectedLabelStyle: TextStyle(fontSize: 13, color: Color.fromRGBO(0, 0, 0, 0.5)),
+            unselectedLabelStyle: TextStyle(fontSize: 12, color: Color.fromRGBO(0, 0, 0, 0.24)),
             items: _tabItems(),
             // 点击切换页面，设置选中的索引
             onTap: (index) {
@@ -65,32 +65,32 @@ class _TabPageState extends State<TabPage> {
     List<BottomNavigationBarItem> items = [];
     items.add(BottomNavigationBarItem(
       label: "首页",
-      icon: Image.asset("images/icon_bar_home.png", width: 32, height: 32),
+      icon: Image.asset("images/icon_bar_home.png", width: 24, height: 24),
       // TODO 选中后的图标
       // NavbarItem是自定义的组件，用于设置选中后的图标，做了一个动画效果
       activeIcon: NavbarItem(builder: (context) {
-        return Image.asset("images/icon_bar_home.png", width: 32, height: 32);
+        return Image.asset("images/icon_bar_home.png", width: 24, height: 24);
       }),
     ));
     items.add(BottomNavigationBarItem(
       label: "分类",
-      icon: Image.asset("images/icon_bar_category.png", width: 32, height: 32),
+      icon: Image.asset("images/icon_bar_category.png", width: 24, height: 24),
       activeIcon: NavbarItem(builder: (context) {
-        return Image.asset("images/icon_bar_category.png", width: 32, height: 32);
+        return Image.asset("images/icon_bar_category.png", width: 24, height: 24);
       })
     ));
     items.add(BottomNavigationBarItem(
       label: "通知",
-      icon: Image.asset("images/icon_bar_notify.png", width: 32, height: 32),
+      icon: Image.asset("images/icon_bar_notify.png", width: 24, height: 24),
       activeIcon: NavbarItem(builder: (context) {
-        return Image.asset("images/icon_bar_notify.png", width: 32, height: 32);
+        return Image.asset("images/icon_bar_notify.png", width: 24, height: 24);
       })
     ));
     items.add(BottomNavigationBarItem(
       label: "我的",
-      icon: Image.asset("images/icon_bar_mine.png", width: 32, height: 32),
+      icon: Image.asset("images/icon_bar_mine.png", width: 24, height: 24),
       activeIcon: NavbarItem(builder: (context) {
-        return Image.asset("images/icon_bar_mine.png", width: 32, height: 32);
+        return Image.asset("images/icon_bar_mine.png", width: 24, height: 24);
       })
     ));
     return items;
